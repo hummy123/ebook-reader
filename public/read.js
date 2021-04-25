f21a69dfe92c8ce94a9b9b4b38f90158eb4ea5a3
shahidh7 <shahidh7@uni.coventry.ac.uk>
2021-04-25 23:52:37 +0100

    properly close and open divs at expected time

--- public/read.js
+++ public/read.js
@@ -4,23 +4,110 @@ window.addEventListener('DOMContentLoaded', () => {
 	
 	//add event listeners for various buttons once document loaded
 	document.getElementById("copyButton").addEventListener("click", copyText)
-	document.getElementById("idiomButton").addEventListener("click", idiomPredict)
 	document.getElementById("newnoteButton").addEventListener("click", noteForm)
 	document.getElementById('saveNote').addEventListener('click', addNote)
 	document.getElementById('viewNotesButton').addEventListener('click', viewNotes)
+	document.getElementById("idiomButton").addEventListener("click", idiomPredict)
 
+	document.getElementById('settingsButton').addEventListener('click', openSettings)
+	document.getElementById('layerOpacity').addEventListener('input', changeShade)
+	document.getElementById('colourOverlay').addEventListener('input', changeShade)
+
+	document.getElementById('stopClick').addEventListener('click', closeAll)
+	
 	document.getElementById("menu").onmousedown = function(e) {
 		e = e || window.event
 		e.preventDefault()
 	}
 
-	document.body.onmousedown = function(e) {
-		closeMessage()
-	}
+
+	/* EXAMPLE CODE OF GETTING ALL P ELEMENTS FOR DISPLAY SETTINGS
+	var x = document.querySelectorAll("main");
+
+	for (test of x) {
+		test.style.backgroundColor = "red"; 
+	}*/
+	
 
 	//add bookmark for most recently opened page
 	addBookmark()
 })
+
+function closeAll() {
+	closeStopDiv()
+	closeMenu()
+	closeNote()
+	closeSettings()
+	closeMessage()
+}
+
+function closeSettings() {
+	const settingsDiv = document.getElementById("display")
+	settingsDiv.style.display = 'none'
+}
+
+function closeStopDiv() {
+	const stopDiv = document.getElementById("stopClick")
+	stopDiv.style.display = "none"
+}
+
+function openStopDiv() {
+	const stopDiv = document.getElementById("stopClick")
+	
+    stopDiv.style.top = 0 + 'px'
+    stopDiv.style.left = 0  + 'px'
+	stopDiv.style.height = "100%"
+	stopDiv.style.width = "100%"
+	stopDiv.style.boxShadow = 'none'
+	stopDiv.style.position = 'fixed'
+	stopDiv.style.backgroundColor = 'transparent'
+	stopDiv.style.display = "block"
+	stopDiv.style.zIndex = '1'
+}
+
+function openSettings(){
+	closeAll()
+	openStopDiv()
+	const settingsDiv = document.getElementById("display")
+	
+	const divWidth = (window.innerWidth / 4)
+	const divHeight = (window.innerHeight * 0.2)
+
+    settingsDiv.style.top = divHeight + 'px'
+    settingsDiv.style.left = divWidth  + 'px'
+	settingsDiv.style.height = "fit-contents"
+	settingsDiv.style.width = "50%"
+	settingsDiv.style.position = 'fixed'
+	settingsDiv.style.display = "block"
+	settingsDiv.style.zIndex = '2'
+}
+
+function changeShade() {
+	const layerDiv = document.getElementById('lightLayer')
+
+	let layerOpacityVal = document.getElementById('layerOpacity').value
+	layerOpacityVal = layerOpacityVal / 100
+	let color = document.getElementById('colourOverlay').value
+
+	if (color === 'white') {
+		color = '255,255,255'
+	} else if (color === 'grey') {
+		color = '190,190,190'
+	} else if (color === 'black') {
+		color = '0,0,0'
+	}
+	
+	layerDiv.style.top = 0 + 'px'
+    layerDiv.style.left = 0  + 'px'
+	layerDiv.style.width = '100vw'
+	layerDiv.style.height = '100vh'
+	layerDiv.style.backgroundColor = `rgba(${color},${layerOpacityVal})`
+	layerDiv.style.position = 'fixed'
+	layerDiv.style.display = "block"
+	layerDiv.style.boxShadow = 'none'
+	layerDiv.style.pointerEvents = 'none'
+	layerDiv.style.zIndex = '3'
+}
 
 async function addBookmark() {
 	let noteLink = window.location.href
@@ -33,7 +120,6 @@ async function addBookmark() {
 
 async function idiomPredict() {
 	const text = window.getSelection().toString()
-	closeMenu()
 	let formData = new FormData()
 	formData.append('sentence', text)
 
@@ -49,6 +135,8 @@ async function idiomPredict() {
 }
 
 function displayMessage(messageHeading, messageContents) {
+	closeAll()
+	openStopDiv()
 	const msgDiv = document.getElementById("messages")
 	
 	const msgWidth = (window.innerWidth * 0.35)
@@ -60,6 +148,7 @@ function displayMessage(messageHeading, messageContents) {
 	msgDiv.style.width = "30%"
 	msgDiv.style.position = 'fixed'
 	msgDiv.style.display = "block"
+	msgDiv.style.zIndex = '2'
 
 	const heading = document.getElementById("messageHeading")
 	const contents = document.getElementById("messageContents")
@@ -69,8 +158,7 @@ function displayMessage(messageHeading, messageContents) {
 
 function copyText() {
 	document.execCommand("copy")
-	closeMenu()
-	closeNote()
+	closeAll()
 	deselect()
 }
 
@@ -83,7 +171,7 @@ function openMenu(selection) {
 		menu.style.left = (bound[bound.length-1].right	 + 3).toString() + 'px'
 		menu.style.position = 'fixed'
 		menu.style.display = "block"
-		openMenu()
+		menu.style.zIndex = '2'
 	}
 	else {
 		closeMenu()
@@ -101,7 +189,7 @@ function closeNote() {
 }
 
 function noteForm() {
-	closeMenu()
+	closeAll()
 	const noteForm = document.getElementById('notes')
 	
 	const highlighted = document.getElementById('highlighted')
@@ -117,9 +205,12 @@ function noteForm() {
 	noteForm.style.position = 'fixed'
 	noteForm.style.display = "block"
 	noteForm.style.padding = "2em"
+	noteForm.style.zIndex = "2"
+	openStopDiv()
 }
 
 async function addNote() {
+	closeAll()
 	const highlighted = document.getElementById('highlighted')
 	highlightedText = highlighted.value
 
@@ -136,12 +227,11 @@ async function addNote() {
 	method: 'POST', 
 	body: formData
 	})
-	closeNote()
 	displayMessage('Note added successfully.', `Saved note "${noteText}" for text "${highlightedText}".`)
 }
 
 async function viewNotes() {
-	closeMenu()
+	closeAll()
 
 	let noteLink = window.location.href
 	noteLink = noteLink.substr(0, noteLink.lastIndexOf("\/") + 1)
@@ -150,7 +240,6 @@ async function viewNotes() {
 	
 	const response = await data.json()
 	const table = await noteTable(response)
-	console.log(table)
 	displayMessage("View Notes", table)
 }
 
@@ -173,10 +262,8 @@ function deselect() {
 }
 
 document.addEventListener('scroll', function(e) {
-	closeMenu()
+	closeAll()
 	deselect()
-	closeNote()
-	closeMessage()
 })
 
 document.addEventListener("selectionchange", function(e) {
